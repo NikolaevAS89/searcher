629f78d7d1101cf0c4fac0a3ebd2a8abd9435e38
k.l.maksimov <k.l.maksimov@tinkoff.ru>
2019-06-24 22:22:01 +0300

    комментарии

--- searcher/src/main/java/ru/timestop/entrance/service/db/ResultEntityService.java
+++ searcher/src/main/java/ru/timestop/entrance/service/db/ResultEntityService.java
@@ -7,8 +7,11 @@ import ru.timestop.entrance.entities.ResultEntity;
  * @version 1.0.0
  * @since 29.09.2018
  */
+//Опять же название тут бы подошло SearchResultStorage
 public interface ResultEntityService {
 
+    // вижу ты не пользуешься этой функцией, это уже мёртвый код, но к тому же она нарушает абстракцию этого сервиса
+    // необходимого исключительно для операций сохранения и получения результатов поиска в БД
     ResultEntity createNew(Integer number);
 
     void registration(ResultEntity result);
